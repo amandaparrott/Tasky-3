8bf6474f31283a3078e0588897f9d34fa37ae908
ChrisFC-github <70590048+ChrisFC-github@users.noreply.github.com>
2020-12-07 15:27:34 -0600

    removing bundlefiles

--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -7,13 +7,13 @@ import Reminders from "./reminders";
 import Messages from "./messages";
 import Tasks from "./tasks";
 
-export const Connection = mysql.createConnection({
-  host: "us-cdbr-east-02.cleardb.com",
-  port: 3306,
-  user: "b742c71a1c0af7",
-  password: "7d6dca5a",
-  database: "heroku_bfe19d473e6aa58",
-});
+  export const Connection = mysql.createConnection({
+    host: "us-cdbr-east-02.cleardb.com",
+    port: 3306,
+    user: "b742c71a1c0af7",
+    password: "7d6dca5a",
+    database: "heroku_bfe19d473e6aa58",
+  });
 
 export const Query = (query: string, values?: Array<string | number>) => {
   return new Promise<Array<any>>((resolve, reject) => {
